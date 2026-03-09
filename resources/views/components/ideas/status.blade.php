93fe298e46fc2ff665fb719b41e7e49986c98515
mariecaron2001 <mariecaron27@gmail.com>
2026-03-09 12:08:40 +0100

    Add ideas flitering (first part)

--- resources/views/components/ideas/status.blade.php
+++ resources/views/components/ideas/status.blade.php
@@ -1,23 +1,23 @@
 @props([
     'status' => 'pending'
-]);
+])
 
 @php
     $classes = 'inline-block rounded-full border px-2 py-1 text-xs font-medium';
     
     if($status === 'pending')
     {
-        $classes .= ' bg-yellow-500/10 text-yellow-500 border-yellow-500/20';
+        $classes = ' bg-yellow-500/10 text-yellow-500 border-yellow-500/20';
     }
 
     elseif($status === 'in_progress')
     {
-        $classes .= ' bg-blue-500/10 text-blue-500 border-blue-500/20';
+        $classes = ' bg-blue-500/10 text-blue-500 border-blue-500/20';
     }
 
     elseif($status === 'completed')
     {
-        $classes .= ' bg-green-500/10 text-green-500 border-green-500/20';
+        $classes = ' bg-green-500/10 text-green-500 border-green-500/20';
     }
 
 @endphp
--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -5,6 +5,14 @@
             <p class="text-muted-foreground text-sm mt-2">Capture your thoughts. Make a plan.</p>
         </header>
 
+        <div>
+            <a href="/ideas" class="btn {{ request()->has('status') ? 'btn-outlined' : ' ' }}">All</a>
+
+            @foreach (App\IdeaStatus::cases() as $status)
+                <a href="/ideas?status={{ $status->value }}" class="btn {{ request('status') === $status->value ? ' ' : 'btn-outlined' }}">{{ $status->label() }}</a>
+            @endforeach
+        </div>
+
         <div class="mt-10 text-muted-foreground">
           <div class="grid md:grid-cols-2 gap-6">
                 @forelse ($ideas as $idea)
@@ -12,9 +20,10 @@
                     <h3 class="text-foreground text-lg"> {{ $idea->title }} </h3>
 
                     <div class="mt-1">
-<x-ideas.status :status="$idea->status">
-    {{ $idea->status->label() }}
-</x-ideas.status>                    </div>
+                        <x-ideas.status status="{{ $idea->status }}">
+                            {{ $idea->status->label() }}
+                        </x-ideas.status>                    
+                    </div>
 
                     <div class="mt-5 line-clamp-3">{{ $idea->description }}</div>
                     <div class="mt-4">{{ $idea->created_at->diffForHumans() }}</div>
