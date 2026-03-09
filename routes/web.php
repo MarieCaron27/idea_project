8f397737911e5d245c5d0e0c077e470518762e0b
mariecaron2001 <mariecaron27@gmail.com>
2026-03-09 11:23:43 +0100

    Add ideas cards

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionsController;
 
-Route::get('/', fn () => view('welcome'));
+//Route::get('/', fn () => view('welcome'));
+Route::redirect('/','/ideas');
+
+//Get all ideas
+Route::get('/ideas',[IdeaController::class, 'index'])->name('idea.index')->middleware('auth');
+
+//Get one idea
+Route::get('/ideas/{idea}',[IdeaController::class, 'show'])->name('idea.show')->middleware('auth');
 
 //Create User
 Route::get('/auth/register', [RegisteredUserController::class, 'create'])->middleware('guest');
@@ -13,7 +21,7 @@ Route::get('/auth/register', [RegisteredUserController::class, 'create'])->middl
 Route::post('/auth/register', [RegisteredUserController::class, 'store'])->middleware('guest');
 
 //Create User's session
-Route::get('/auth/login', [SessionsController::class, 'create'])->middleware('guest');
+Route::get('/auth/login', [SessionsController::class, 'create'])->name('login')->middleware('guest');
 
 //Store User's session
 Route::post('/auth/login', [SessionsController::class, 'store'])->middleware('guest');
